f22cde3cbab43a0630ceac12ce8662840863f9ac
Timi-Artturi Mäkelä <timi.artturi.makela@gmail.com>
2019-03-01 14:03:56 +0200

    Version bump

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Geniem Roles
  * Plugin URI: https://github.com/devgeniem/wp-geniem-roles
  * Description: WordPress plugin to edit and create roles in code.
- * Version: 1.0.1
+ * Version: 1.0.3
  * Author: Timi-Artturi Mäkelä / Anttoni Lahtinen / Ville Siltala / Geniem Oy
  * Author URI: https://geniem.fi
  */
